76c4206bb7c845635978716c4e85b15a51f46f61
niccolosea0 <niccolosea@gmail.com>
2026-02-13 18:45:23 +0400

    Add EvenIntegers task

---
+++ level-1/level-1.4/EvenIntegers.java
@@ -0,0 +1,11 @@
+// Task N1:  Print to the console all even numbers from 1 to 100.
+public class EvenIntegers {
+    public static void main(String[] args) {
+         
+         for (int i = 0; i < 100; i++) {
+             if (i % 2 == 0) {
+                 System.out.println(i );
+             }
+         }
+    }
+}
