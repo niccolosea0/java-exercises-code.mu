33c7aab399056dda81ad3b5fca06bca029725d5c
niccolosea0 <niccolosea@gmail.com>
2026-02-14 08:46:28 +0400

    Add convert.java task

---
+++ level-1/level-1.5/Convert.java
@@ -0,0 +1,15 @@
+// Given an integer containing the number of kilobytes:
+// int kb = 35;
+// Convert this value to bytes.
+
+public class Convert {
+    public static void main(String[] args) {
+
+        int kb = 35;
+
+        byte num = (byte) kb;
+
+        System.out.println("Converted num: " + num);
+
+    }
+}
