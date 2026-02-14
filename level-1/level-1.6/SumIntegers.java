245eb80febc9bc286bc5391dca03959545db1d03
niccolosea0 <niccolosea@gmail.com>
2026-02-14 08:52:41 +0400

    Add sum of all itnegers from 1 to 100

---
+++ level-1/level-1.6/SumIntegers.java
@@ -0,0 +1,15 @@
+// Task N1: Find the sum of all integers from 1 to 100.
+
+public class SumIntegers {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of all integers from 1 to 100: " + sum);
+         
+    }
+}
+
