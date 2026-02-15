70a6c0eb769312b2b640d2c824edfeb4bb5fe733
niccolosea0 <niccolosea@gmail.com>
2026-02-15 10:09:50 +0400

    Add Convert task

---
+++ level-1/level-1.6/Convert.java
@@ -0,0 +1,14 @@
+// Given an integer containing the number of megabytes:
+// 
+// int mb = 35;
+// Convert this value to bytes.
+
+public class Convert {
+    public static void main(String[] args) {
+
+        int mb = 35;
+        double gb = 35 / 1000.;
+        System.out.println("35 mb is " + gb + " gb");
+         
+    }
+}
