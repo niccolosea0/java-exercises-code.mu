0dae0bc9ea9c77460c505d22998120ddcb86cb7c
niccolosea0 <niccolosea@gmail.com>
2026-02-18 11:51:43 +0400

    Add Seconds.java task

---
+++ level-1/level-1.9/Seconds.java
@@ -0,0 +1,12 @@
+// Task N2 Write a program that will find out how many seconds are in a day.
+
+public class Seconds {
+    public static void main(String[] args) {
+
+        int hoursInDay = 24;
+        int minutesInDay = hoursInDay * 60;
+        int secondsInDay = minutesInDay * 60;
+
+        System.out.println("There is " + secondsInDay + " seconds in a day!");
+    }
+}
