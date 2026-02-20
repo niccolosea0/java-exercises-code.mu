350bcd7750235ed92a2b2b7c2ed216d3484e8ffc
niccolosea0 <niccolosea@gmail.com>
2026-02-20 18:25:34 +0400

    Add first task

---
+++ level-2/level-1/Integers.java
@@ -0,0 +1,11 @@
+public class Integers {
+    public static void main(String[] args) {
+         
+         int[] arr = {1, 2, 3, 4, 5};
+
+         for (int elem : arr) {
+             System.out.print(elem + " ");
+         }
+         System.out.println();
+    }
+}
