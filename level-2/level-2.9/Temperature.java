ff4f9ffebacbb8564c676ca70cf198c8869c40a7
niccolosea0 <niccolosea@gmail.com>
2026-03-29 23:42:01 +0400

    Add fourht task of 2.9

---
+++ level-2/level-2.9/Temperature.java
@@ -0,0 +1,11 @@
+public class Temperature {
+    public static void main(String[] args) {
+
+        int tempC = 25;
+        System.out.println("Temperature in degrees Celsius: " + tempC);
+
+        double tempF = (tempC * 9/5.) + 32;
+        System.out.println("Temperature in degrees Fahrenheit: " + tempF);
+         
+    }
+}
